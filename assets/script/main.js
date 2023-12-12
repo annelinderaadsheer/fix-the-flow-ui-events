014e2e6c30720eda960b804296aacd2a4913833e
annelinderaadsheer <144004885+annelinderaadsheer@users.noreply.github.com>
2023-12-12 14:58:40 +0100

    Button 1 en 2

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,20 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+// Button Frontend Click Event
+
+let frontend = document.querySelector('#frontend')
+
+frontend.addEventListener("click", function () {
+  frontend.classList.add("changeColor");
+})
+
+// Button Design Dubble Click Event
+
+let design = document.querySelector('#design')
+
+design.addEventListener("dblclick", function () {
+  design.classList.add("changeColor");
+})
